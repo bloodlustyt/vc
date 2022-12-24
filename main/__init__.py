4729c6a44213c8ac358a9e13b761ab0a14bb6d57
bloodlustyt <67737806+bloodlustyt@users.noreply.github.com>
2022-12-24 15:47:57 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -7,9 +7,9 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-BOT_UN = config("BOT_UN", default=None)
+API_ID = 28533415
+API_HASH = "96332cd303a9277980b5dd0274568b36"
+BOT_TOKEN = "5882308938:AAFfDSzIARrQsN6Lf_-GoDZPv4Vfs5xHVvI"
+BOT_UN = "For_Batak"
 
 Drone = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
