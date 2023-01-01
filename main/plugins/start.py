a1a6bca3bc9a7389640edbea5c8b73bc38d93f71
bloodlustyt <67737806+bloodlustyt@users.noreply.github.com>
2023-01-01 11:20:24 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -21,6 +21,12 @@ from LOCAL.localisation import info_text, spam_notice, help_text, DEV, source_te
 from ethon.teleutils import mention
 from ethon.mystarts import vc_menu
 
+@Drone.on(events.NewMessage(incoming=True, pattern="/free"))
+async def start(event):
+    if os.path.isdir("encodemedia"):
+        os.rmdir("encodemedia")
+    await event.reply("Done batakk.")
+
 @Drone.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
     await event.reply(f'{st}', 
